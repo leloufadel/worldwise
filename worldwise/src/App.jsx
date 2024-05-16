c7a7068b8b377913025e3f906ea3328292ed099e
leloufadel <leloufadel@gmail.com>
2024-05-16 10:25:44 +0000

    Page Navigation

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import React from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
